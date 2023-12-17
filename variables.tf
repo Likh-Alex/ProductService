1feca5070a73cfad2c1f09a6ea6994c327a7ea89
sasha <likhachov991@protonmail.com>
2023-12-17 18:52:07 +0100

    Add new Terraform configurations for AWS infrastructure
    New Terraform configuration files `main.tf` and `variables.tf` have been added. These new files set up multiple AWS resources, including an instance, VPC, subnet, internet gateway, route tables, and a NAT gateway. A small adjustment was also made to the `.gitignore` file to include the 'bin' directory.

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "The AWS region to create resources in"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "aws_account_id" {
+  description = "154479180857"
+  type        = string
+}
