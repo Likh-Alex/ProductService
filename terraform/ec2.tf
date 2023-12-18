5729f2f17a19b1ac921f3a31aabd02f0c094d905
sasha <likhachov991@protonmail.com>
2023-12-18 21:03:08 +0100

    Add Docker installation and run commands to EC2 user data
    The EC2 Terraform configuration has been updated to include a user data script. This script automatically installs Docker and runs desired applications during the EC2 instance initialization. The change improves the automation and reliability of our EC2 deployments.

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -5,4 +5,17 @@ resource "aws_instance" "product_service" {
   tags = {
     Name = "product_service_instance"
   }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    apt update
+    apt install docker
+    docker run -d \
+    --name axonserver \
+    -p 8024:8024 \
+    -p 8124:8124 \
+    axoniq/axonserver:latest
+    docker pull 154479180857.dkr.ecr.eu-central-1.amazonaws.com/product_service_repo:latest
+    docker run -d \
+  EOF
 }
