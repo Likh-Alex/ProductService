d2abf96168420e32761af8b6d9ccfcb4fd0828ff
sasha <likhachov991@protonmail.com>
2023-12-19 21:00:34 +0100

    Refactor terraform code and rename workflows
    Updated the AWS API Gateway Terraform configurations and renamed GitHub workflows for better clarity and understanding. Revised the Terraform configurations related to AWS API Gateway to have more meaningful names and improved structure. Also, the GitHub workflows are renamed to be more intuitive and to better reflect their respective functions and roles.

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,51 +1,55 @@
-resource "aws_api_gateway_rest_api" "my_api" {
+resource "aws_api_gateway_rest_api" "product_service_api" {
   name        = "ProductServiceAPI"
   description = "API for Product Service"
 }
 
 resource "aws_api_gateway_resource" "products_resource" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
+  parent_id   = aws_api_gateway_rest_api.product_service_api.root_resource_id
   path_part   = "products"
+  rest_api_id = aws_api_gateway_rest_api.product_service_api.id
+}
+
+resource "aws_api_gateway_method" "products_post" {
+  rest_api_id   = aws_api_gateway_rest_api.product_service_api.id
+  http_method   = "POST"
+  resource_id   = aws_api_gateway_resource.products_resource.id
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "products_post_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.product_service_api.id
+  resource_id             = aws_api_gateway_resource.products_resource.id
+  http_method             = aws_api_gateway_method.products_post.http_method
+  type                    = "HTTP"
+  uri                     = "http://${aws_instance.product_service.public_ip}/products"
+  depends_on              = [aws_api_gateway_method.products_post]
+  integration_http_method = "POST"
 }
 
 resource "aws_api_gateway_method" "products_get" {
-  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  rest_api_id   = aws_api_gateway_rest_api.product_service_api.id
   resource_id   = aws_api_gateway_resource.products_resource.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method" "products_post" {
-  rest_api_id   = aws_api_gateway_rest_api.my_api.id
-  resource_id   = aws_api_gateway_resource.products_resource.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
 resource "aws_api_gateway_integration" "products_get_integration" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  resource_id = aws_api_gateway_resource.products_resource.id
-  http_method = "GET"
-  type        = "HTTP"
-  uri         = "http://${aws_instance.product_service.public_ip}/products"
-  depends_on  = [aws_api_gateway_method.products_get]
-}
-
-resource "aws_api_gateway_integration" "products_post_integration" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  resource_id = aws_api_gateway_resource.products_resource.id
-  http_method = "POST"
-  type        = "HTTP"
-  uri         = "http://${aws_instance.product_service.public_ip}/products"
-  depends_on  = [aws_api_gateway_method.products_post]
+  rest_api_id             = aws_api_gateway_rest_api.product_service_api.id
+  resource_id             = aws_api_gateway_resource.products_resource.id
+  http_method             = aws_api_gateway_method.products_get.http_method
+  type                    = "HTTP"
+  uri                     = "http://${aws_instance.product_service.public_ip}/products"
+  depends_on              = [aws_api_gateway_method.products_get]
+  integration_http_method = "GET"
 }
 
 resource "aws_api_gateway_deployment" "api_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  rest_api_id = aws_api_gateway_rest_api.product_service_api.id
   stage_name  = "prod"
   depends_on  = [
     aws_api_gateway_integration.products_get_integration,
-    aws_api_gateway_integration.products_post_integration
+    aws_api_gateway_method.products_get,
+    aws_api_gateway_integration.products_post_integration,
+    aws_api_gateway_method.products_post
   ]
 }
