e4da4740f658a68ae685b7d9b1e97eb7604f7a97
sasha <likhachov991@protonmail.com>
2023-12-19 21:44:07 +0100

    Refine EC2 instance configuration and add SSH key pair in Terraform
    Terraform configuration files have been updated. A new SSH key pair is added and associated with the EC2 instance for better accessibility. Furthermore, the EC2 instance's user data script is refined to improve the setup process, and an output for the instance's public IP is added for easier access. Changes to the API Gateway integration dependencies and Github Actions are minimal adjustments for better formatting and process definition.

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "product_service" {
-  ami           = "ami-0669b163befffbdfc"
-  instance_type = "t2.micro"
+  ami                         = "ami-0669b163befffbdfc"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.ssh_key.key_name
+  associate_public_ip_address = true
+
 
   tags = {
     Name = "product_service_instance"
@@ -8,14 +11,21 @@ resource "aws_instance" "product_service" {
 
   user_data = <<-EOF
     #!/bin/bash
-    apt update
-    apt install docker
-    docker run -d \
-    --name axonserver \
-    -p 8024:8024 \
-    -p 8124:8124 \
-    axoniq/axonserver:latest
+    apt update -y
+    apt install -y docker.io
+    systemctl start docker
+    systemctl enable docker
+
+    $(aws ecr get-login --no-include-email --region eu-central-1)
+
+    docker run -d --name axonserver -p 8024:8024 -p 8124:8124 axoniq/axonserver:latest
+
     docker pull 154479180857.dkr.ecr.eu-central-1.amazonaws.com/product_service_repo:latest
-    docker run -d \
+    docker run -d --name my_product_service -p [YourPort]:[YourContainerPort] 154479180857.dkr.ecr.eu-central-1.amazonaws.com/product_service_repo:latest
   EOF
+}
+
+output "instance_ip" {
+  description = "The public ip for ssh access"
+  value       = aws_instance.product_service.public_ip
 }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,2 +1,2 @@
 aws_account_id = "154479180857"
-aws_region = "eu-central-1"
+aws_region     = "eu-central-1"
