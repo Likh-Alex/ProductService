348ddd4bcc90f6b968a6324338ee4973abd53310
sasha <likhachov991@protonmail.com>
2023-12-31 17:40:33 +0100

    Update Axon server Docker run command in user_data.sh
    The changes introduced add volume mounts to the Axon server Docker run command in the Terraform file user_data.sh. The inclusion of these persistent volume mounts ensures consistent storage across the docker container's lifecycle.

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -7,7 +7,14 @@
   systemctl enable docker
 
   # Pull and run the Axon server
-  docker run -d --name axonserver -p 8024:8024 -p 8124:8124 axoniq/axonserver:latest
+  docker run -d --name axonserver \
+    -p 8024:8024 \
+    -p 8124:8124 \
+    -v axondata:/data \
+    -v axonevents:/eventdata \
+    -v axonconfig:/config \
+    axoniq/axonserver:latest
+
 
   # Pull and run the Product service
   docker run -d --name product-service -p 8081:8081 sasha1doc/cqrs_product_service:latest
