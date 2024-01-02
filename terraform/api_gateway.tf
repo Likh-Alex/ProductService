fadaf1e00a7369c752e0694c3dc348bae02b4773
sasha <likhachov991@protonmail.com>
2024-01-02 19:21:57 +0100

    "Update application YAML and API Gateway configuration"
    In this commit, the application YAML file has been updated to include the server address. Also, the Terraform configuration for API Gateway has been adjusted to use HTTPS protocol and point to a different port, improving secure access and aligning with the recent server changes.

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -21,7 +21,7 @@ resource "aws_api_gateway_integration" "products_post_integration" {
   resource_id             = aws_api_gateway_resource.products_resource.id
   http_method             = aws_api_gateway_method.products_post.http_method
   type                    = "HTTP"
-  uri                     = "http://${aws_instance.product_service.public_ip}/products"
+  uri                     = "https://${aws_instance.product_service.public_ip}:8081/products"
   depends_on              = [aws_api_gateway_method.products_post]
   integration_http_method = "POST"
 }
@@ -38,7 +38,7 @@ resource "aws_api_gateway_integration" "products_get_integration" {
   resource_id             = aws_api_gateway_resource.products_resource.id
   http_method             = aws_api_gateway_method.products_get.http_method
   type                    = "HTTP"
-  uri                     = "http://${aws_instance.product_service.public_ip}/products"
+  uri                     = "https://${aws_instance.product_service.public_ip}:8081/products"
   depends_on              = [aws_api_gateway_method.products_get]
   integration_http_method = "GET"
 }
