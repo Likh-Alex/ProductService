c1f056a4f1aae087f1c90a1b0b9b22b26979dd47
sasha <likhachov991@protonmail.com>
2024-01-02 10:42:53 +0100

    Create custom Docker network and adjust startup sequence
    Modified the user_data.sh file to include the creation of a custom Docker network named "axon-network". Also reordered commands to guarantee that the Axon Server is ready before starting the product service. Additionally, added an axonserver address in the application.properties for proper server connection.

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-  yum update -y
-  yum install -y docker unzip
+#  yum update -y
+#  yum install -y docker unzip
+#
+#  # Start and enable Docker
+#  systemctl start docker
+#  systemctl enable docker
 
-  # Start and enable Docker
-  systemctl start docker
-  systemctl enable docker
+  # Create a Docker network
+  docker network create axon-network
 
-  # Pull and run the Axon server
+  # Pull and run the Axon server on the custom network
   docker run -d --name axonserver \
+    --network axon-network \
     -p 8024:8024 \
     -p 8124:8124 \
     -v axondata:/data \
@@ -15,13 +19,10 @@
     -v axonconfig:/config \
     axoniq/axonserver:latest
 
-  # Pull and run the Product service
-  docker run -d --name product-service -p 8081:8081 sasha1doc/cqrs_product_service:latest
-
   # Wait for Axon Server to be ready
   echo "Waiting for Axon Server to be ready..."
   until $(curl --output /dev/null --silent --head --fail http://localhost:8024); do
-    printf 'Waiting axonserver to start...\n'
+    printf 'Waiting for Axon Server to start...\n'
     sleep 5
   done
 
@@ -29,3 +30,10 @@
 
   # Initialize Axon Server using REST API
   curl -X POST http://localhost:8024/v1/context/init?context=default
+
+  # Pull and run the Product service on the custom network
+  docker run -d --name product-service \
+    --network axon-network \
+    -p 8081:8081 \
+    -e SPRING_PROFILES_ACTIVE=prod \
+    sasha1doc/cqrs_product_service:latest
