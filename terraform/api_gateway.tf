effd2af3ed52cc4cd18cd2cb05c88108e8bd23e2
sasha <likhachov991@protonmail.com>
2024-01-05 21:41:21 +0100

    Add IAM policy and attachment to Terraform configuration
    Added an IAM policy to allow apigateway GET operations, and attached this policy to an existing IAM role within Terraform script for AWS. Reformatted dependencies and log settings for improved readability. These changes align with security best practices, providing specific resource access via IAM policies.

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -45,7 +45,7 @@ resource "aws_api_gateway_integration" "products_get_integration" {
 
 resource "aws_api_gateway_deployment" "api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.product_service_api.id
-  depends_on = [
+  depends_on  = [
     aws_api_gateway_integration.products_get_integration,
     aws_api_gateway_method.products_get,
     aws_api_gateway_integration.products_post_integration,
@@ -127,7 +127,7 @@ resource "aws_api_gateway_stage" "api_stage" {
 
   access_log_settings {
     destination_arn = aws_cloudwatch_log_group.api_gateway_log_group.arn
-    format = jsonencode({
+    format          = jsonencode({
       "requestId"      = "$context.requestId",
       "ip"             = "$context.identity.sourceIp",
       "caller"         = "$context.identity.caller",
@@ -151,4 +151,24 @@ resource "aws_api_gateway_method_settings" "example" {
     metrics_enabled = true
     logging_level   = "INFO"
   }
+}
+
+resource "aws_iam_policy" "api_gateway_policy" {
+  name = "api_gateway_policy"
+
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Action" : "apigateway:GET",
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "api_gateway_policy_attachment" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.api_gateway_policy.arn
 }
